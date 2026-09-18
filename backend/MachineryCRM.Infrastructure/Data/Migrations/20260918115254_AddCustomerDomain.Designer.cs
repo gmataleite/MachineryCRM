91da9125ee9421896b7a259ba2a627e810d2fedc
gmataleite <guilherme.leite.dev@gmail.com>
2026-09-18 09:34:00 -0300

    feat(infrastructure): add Customer Domain migration
    refactor DbSeeder and create migrations for Customer Domain

--- backend/MachineryCRM.Infrastructure/Data/Migrations/20260918115254_AddCustomerDomain.Designer.cs
+++ backend/MachineryCRM.Infrastructure/Data/Migrations/20260918115254_AddCustomerDomain.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MachineryCRM.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MachineryCRM.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260918115254_AddCustomerDomain")]
+    partial class AddCustomerDomain
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
